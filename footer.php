531fa4231492a9b7fb5ae167c0bf6c467e1c4848
vishal <vishalthomabare3131@gmail.com>
2024-09-23 11:03:10 +0530

    fruit

---
+++ footer.php
@@ -0,0 +1,104 @@
+<section class="footer4 section-tb-padding ">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="home4-footer">
+                            <div class="f-logo">
+                                <ul class="footer-ul">
+                                    <li class="footer-li footer-logo">
+                                        <a href="index6.html">
+                                            <img class="img-fluid" src="image/logo4.png" alt="">
+                                        </a>
+                                        <p>Tortor neque egestas augue, eu vulputate magna eros eu erat.</p>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)"><img src="image/payment.png" class="img-fluid" alt="pay-image"></a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="footer-bottom">
+                                <div class="footer-link" id="footer-accordian">
+                                    <div class="f-link">
+                                        <h2 class="h-footer">Top search</h2>
+                                        <a href="#t-search" data-bs-toggle="collapse" class="h-footer">
+                                            <span>Top search</span>
+                                            <i class="fa fa-angle-down"></i>
+                                        </a>
+                                        <ul class="f-link-ul collapse" id="t-search" data-bs-parent="#footer-accordian">
+                                            <li class="f-link-ul-li"><a href="product-style-6.html">Fruits</a></li>
+                                            <li class="f-link-ul-li"><a href="product-style-6.html">Fast food</a></li>
+                                            <li class="f-link-ul-li"><a href="product-style-6.html">Vegetable</a></li>
+                                            <li class="f-link-ul-li"><a href="product-style-6.html">Salads</a></li>
+                                            <li class="f-link-ul-li"><a href="product-style-6.html">Bestseller</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="f-link">
+                                        <h2 class="h-footer">Services</h2>
+                                        <a href="#t-cate" data-bs-toggle="collapse" class="h-footer">
+                                            <span>Services</span>
+                                            <i class="fa fa-angle-down"></i>
+                                        </a>
+                                        <ul class="f-link-ul collapse" id="t-cate" data-bs-parent="#footer-accordian">
+                                            <li class="f-link-ul-li"><a href="about-us.html">About vegist</a></li>
+                                            <li class="f-link-ul-li"><a href="faq%27s.html">Faq's</a></li>
+                                            <li class="f-link-ul-li"><a href="contact.html">Contact us</a></li>
+                                            <li class="f-link-ul-li"><a href="blog-style-6-3-grid.html">News</a></li>
+                                            <li class="f-link-ul-li"><a href="sitemap.html">Sitemap</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="f-link">
+                                        <h2 class="h-footer">Privacy & aerms</h2>
+                                        <a href="#services" data-bs-toggle="collapse" class="h-footer">
+                                            <span>Privacy & terms</span>
+                                            <i class="fa fa-angle-down"></i>
+                                        </a>
+                                        <ul class="f-link-ul collapse" id="services" data-bs-parent="#footer-accordian">
+                                            <li class="f-link-ul-li"><a href="payment-policy.html">Payment policy</a></li>
+                                            <li class="f-link-ul-li"><a href="privacy-policy.html">Privacy policy</a></li>
+                                            <li class="f-link-ul-li"><a href="return-policy.html">Return policy</a></li>
+                                            <li class="f-link-ul-li"><a href="shipping-policy.html">Shipping policy</a></li>
+                                            <li class="f-link-ul-li"><a href="terms-conditions.html">Terms conditions</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="f-link">
+                                        <h2 class="h-footer">My account</h2>
+                                        <a href="#privacy" data-bs-toggle="collapse" class="h-footer">
+                                            <span>My account</span>
+                                            <i class="fa fa-angle-down"></i>
+                                        </a>
+                                        <ul class="f-link-ul collapse" id="privacy" data-bs-parent="#footer-accordian">
+                                            <li class="f-link-ul-li"><a href="account.html">My account</a></li>
+                                            <li class="f-link-ul-li"><a href="cart-3.html">My cart</a></li>
+                                            <li class="f-link-ul-li"><a href="tracking.html">Order history</a></li>
+                                            <li class="f-link-ul-li"><a href="wishlist.html">My wishlist</a></li>
+                                            <li class="f-link-ul-li"><a href="addresses.html">My address</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="contact-6">
+                                <div class="f-deal-content">
+                                    <h2>Location</h2>
+                                </div>
+                                <ul class="f-contact">
+                                    <li><i class="ion-ios-location"></i></li>
+                                    <li class="contact-link">
+                                        <p>401 Broadway 24th floor</p>
+                                        <p>Near ant mall cross road</p>
+                                    </li>
+                                </ul>
+                                <ul class="f-contact">
+                                    <li><i class="ion-ios-telephone"></i></li>
+                                    <li class="contact-link">
+                                        <a href="tel:1-800-222-000">Phone: 1-800-222-000</a>
+                                        <a href="mailto:demo@demo.com">Email: demo@demo.com</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+      
+                  
